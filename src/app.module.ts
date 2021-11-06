c982053136345fe9b9656181580828f115b6ea1c
NikRuchkin1 <NikRuchkin1>
2021-11-07 00:27:38 +0300

    Finish test nestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductsModule, 
+    MongooseModule.forRoot('mongodb+srv://Nik:1234@cluster0.u4clc.mongodb.net/products?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
